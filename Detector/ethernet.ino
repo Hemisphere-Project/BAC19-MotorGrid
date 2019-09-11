4006849c28fbc0cde50fc0e664713916937c483c
Jeremie <JeremieForge@users.noreply.github.com>
2019-09-11 16:47:00 +0200

    dt

--- Detector/ethernet.ino
+++ Detector/ethernet.ino
@@ -2,72 +2,8 @@
 #include <ArduinoOTA.h>
 
 static bool eth_connected = false;
-
-void ethernet_setup() {
-
-    // LED inbuilt
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(100);
-    digitalWrite(LED_BUILTIN, LOW);
-
-    // STATIC IP
-    IPAddress localIP(10, 0, groupid, nodeid);
-    IPAddress gateway(10, 0, 0, 1);
-    IPAddress subnet(255, 255, 0, 0);
-    IPAddress dns1(8, 8, 8, 8);
-    IPAddress dns2(8, 8, 4, 4);
-
-    // OTA SETUP
-    String devicename = "detector-" + String(groupid)+"."+String(nodeid) + "-v" + String(MG_VERSION, 2);
-    ArduinoOTA.setHostname(devicename.c_str());
-
-    // ETHERNET CONNECT
-    WiFi.onEvent(WiFiEvent);
-    ETH.begin();
-    ETH.config(localIP, gateway, subnet, dns1, dns2);
-
-    rest_setup();
-    osc_setup();
-
-    xTaskCreatePinnedToCore(
-      ethernet_task, /* Function to implement the task */
-      "networkTask", /* Name of the task */
-      15000,  /* Stack size in words */
-      NULL,  /* Task input parameter */
-      1,  /* Priority of the task */
-      NULL,  /* Task handle. */
-      0); /* Core where the task should run */
-}
-
-void ethernet_task( void * parameter) {
-
-    while(true) {
-        if (eth_connected) {
-
-            ArduinoOTA.handle();
-            // osc_loop();
-            // rest_loop();
-            
-            delay(10);
-        }
-        else delay(100);
-    }
-
-    vTaskDelete(NULL);
-}
-
-void ethernet_connected() {
-
-    ArduinoOTA.begin();
-    Serial.println("ArduinoOTA started");
-
-    osc_begin();
-    rest_begin();
-
-    digitalWrite(LED_BUILTIN, HIGH);
-}
-
+static int groupid = 0;
+static int nodeid = 100;
 
 void WiFiEvent(WiFiEvent_t event)
 {
@@ -83,7 +19,7 @@ void WiFiEvent(WiFiEvent_t event)
     case SYSTEM_EVENT_ETH_GOT_IP:
       if (eth_connected) return;
       eth_connected = true;
-      
+
       Serial.print("ETH MAC: ");
       Serial.print(ETH.macAddress());
       Serial.print(", IPv4: ");
@@ -108,4 +44,70 @@ void WiFiEvent(WiFiEvent_t event)
     default:
       break;
   }
+}
+
+
+void ethernet_setup() {
+
+    // LED inbuilt
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+
+    // STATIC IP
+    IPAddress localIP(10, 0, groupid, nodeid);
+    IPAddress gateway(10, 0, 0, 1);
+    IPAddress subnet(255, 255, 0, 0);
+    IPAddress dns1(8, 8, 8, 8);
+    IPAddress dns2(8, 8, 4, 4);
+
+    // OTA SETUP
+    String devicename = "detector-" + String(groupid)+"."+String(nodeid) + "-v" + String(0, 2);
+    ArduinoOTA.setHostname(devicename.c_str());
+
+    // ETHERNET CONNECT
+    WiFi.onEvent(WiFiEvent);
+    ETH.begin();
+    ETH.config(localIP, gateway, subnet, dns1, dns2);
+
+    // rest_setup();
+    // osc_setup();
+
+    xTaskCreatePinnedToCore(
+      ethernet_task, /* Function to implement the task */
+      "networkTask", /* Name of the task */
+      15000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      1,  /* Priority of the task */
+      NULL,  /* Task handle. */
+      0); /* Core where the task should run */
+}
+
+void ethernet_task( void * parameter) {
+
+    while(true) {
+        if (eth_connected) {
+
+            ArduinoOTA.handle();
+            // osc_loop();
+            // rest_loop();
+
+            delay(10);
+        }
+        else delay(100);
+    }
+
+    vTaskDelete(NULL);
+}
+
+void ethernet_connected() {
+
+    ArduinoOTA.begin();
+    Serial.println("ArduinoOTA started");
+
+    // osc_begin();
+    // rest_begin();
+
+    digitalWrite(LED_BUILTIN, HIGH);
 }
